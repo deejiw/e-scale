999aa9c539e24cfdbf952727ee0ba66863d2644d
deejiw <30390987+deejiw@users.noreply.github.com>
2020-12-18 11:06:43 +0700

    completed authActions and errorActions

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import React, { useEffect } from 'react'
 import AppNavBar from './components/AppNavBar'
 import ShoppingList from './components/ShoppingList'
 import ItemModal from './components/ItemModal'
@@ -6,9 +6,16 @@ import { Container } from 'reactstrap'
 
 import { Provider } from 'react-redux'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { loadUser } from './actions/authActions'
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
   return (
     <Provider store={store}>
       <div className='App'>
